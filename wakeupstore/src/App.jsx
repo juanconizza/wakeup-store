7e5aae825421bec0de098d5dea19a56f667c0ac3
juanconizza <juanconizza@gmail.com>
2023-12-12 15:07:34 -0300

    creamos una navbar con Bootstrap version 1.0

--- wakeupstore/src/App.jsx
+++ wakeupstore/src/App.jsx
@@ -1,25 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "./components/NavBar";
 
 
 
 function App() {
   return (
     <>
-        
+        <Navbar />
      
 
-      <div className="App">
-        <h1 className="mb-5">Vite + React</h1>
-        <div className="card">
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs mt-5">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
     </>
   );
 }
