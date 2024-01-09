80e5c26929cc0cf50ddd637d9c082f044043ecef
juanconizza <juanconizza@gmail.com>
2024-01-09 16:11:11 -0300

    Se modificó el botón de Productos agregando la categoria remeras y gorros y se instalo react router dom

--- wakeupstore/src/components/NavBar.jsx
+++ wakeupstore/src/components/NavBar.jsx
@@ -2,21 +2,16 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { CartWidget } from "./CartWidget";
-import logo from '/imagenes/Logo/logo.png';
+import logo from "/imagenes/Logo/logo.png";
+import Dropdown from "react-bootstrap/Dropdown";
 
-
-
-export const NavBar = ({carrito}) => {
+export const NavBar = ({ carrito }) => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container className="ms-auto">
         <Navbar.Brand>
           <a href="index.html">
-            <img
-              className="logonavbar img-fluid"
-              src={logo}
-              alt="logo"
-            />
+            <img className="logonavbar img-fluid" src={logo} alt="logo" />
           </a>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -25,20 +20,28 @@ export const NavBar = ({carrito}) => {
             <Nav.Link className="me-3" href="#inicio">
               Inicio
             </Nav.Link>
-            <Nav.Link className="me-3" href="#remeras">
-              Remeras
-            </Nav.Link>
+            <Dropdown className="nav-item dropdown">
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                Productos
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdown-menu-end">
+                <Dropdown.Item href="#remeras">Remeras</Dropdown.Item>
+                <Dropdown.Item href="#gorras">Gorras</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             <Nav.Link className="me-3" href="#contacto">
               Contacto
             </Nav.Link>
             <Nav.Link className="me-3" href="#login">
               Registrarse / Iniciar Sesión
             </Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-            <Nav.Link className="ms-auto" href="#carrito"> <CartWidget carrito={carrito}/> </Nav.Link> 
-        </Container>
-        
+          </Nav>
+        </Navbar.Collapse>
+        <Nav.Link className="ms-auto" href="#carrito">
+          <CartWidget carrito={carrito} />
+        </Nav.Link>
+      </Container>
     </Navbar>
   );
 };
